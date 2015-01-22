a518a99e4b7f3a788a495adbe709773ab0da8963
Wakaba <wakaba@suikawiki.org>
2015-01-22 16:28:18 +0900

    path

--- bin/prepare-wikipedia-cache.pl
+++ bin/prepare-wikipedia-cache.pl
@@ -10,7 +10,7 @@ my $pattern = qr{(?:都|道|府|県|市|区|郡|町|村)(?:| \([^()]+\))$};
 
 my $wp_d = dir ($ENV{WP_DATA_DIR} // $root_d->subdir ('local'));
 my $dump_f = $wp_d->file ('xml', 'ja.xml');
-my $cache_d = $wp_d->subdir ('cache');
+my $cache_d = $wp_d->subdir ('cache', 'ja');
 
 my $mx = MediaWikiXML::PageExtractor->new_from_cache_d ($cache_d);
 $mx->save_titles_from_f_if_necessary ($dump_f);
